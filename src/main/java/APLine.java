7707dbc7b121c7c0a584c25b21abd039d4979326
zichenmei <143123810+zichenmei@users.noreply.github.com>
2024-04-22 19:51:19 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,25 @@
 public class APLine
 {
   /* your code here */
+  private int a;
+  private int b;
+  private int c;
   
+  public APLine(int d; int e; int f){
+    a=d;
+    b=e;
+    c=f;
+  }
+  public double getSlope(){
+    double slope=(double)((0-a)/b);
+    return slope;
+  }
+  public boolean inOnLine(int x, int y){
+    int first=a*x;
+    int sec=b*y;
+    if((first+sec+c)==0){
+      return true;
+    }
+    return false;
+  }
 }
